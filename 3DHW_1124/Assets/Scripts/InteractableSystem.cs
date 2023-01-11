388986d4f453a1aa7879e819f33bc6bb4b2350b3
yatzu <113869639+yasmina901017@users.noreply.github.com>
2023-01-11 09:37:12 +0800

    1407

---
+++ 3DHW_1124/Assets/Scripts/InteractableSystem.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace yatzu
+{
+    /// <summary>
+    /// 互動系統 : 偵測玩家是否進入並且執行互動行為
+    /// </summary>
+    public class InteractableSystem : MonoBehaviour
+    {
+        // 3D 物件適用
+        //兩個碰撞物件必須其中一個勾選 Is Trigger
+        //碰撞開始
+        private void OnTriggerEnter(Collider other)
+        {
+            print(other.name);
+        }
+
+        //碰撞結束
+        private void OnTriggerExit(Collider other)
+        {
+            
+        }
+
+        //持續碰撞
+        private void OnTriggerStay(Collider other)
+        {
+            
+        }
+    }
+}
